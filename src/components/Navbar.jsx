d176603d7922b92db3601ae92d4484a2495c17f8
pmwritershark <97102702+pmwritershark@users.noreply.github.com>
2023-04-10 13:14:58 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const navbarStyle={
     backgroundColor: "#16453e",
     align:"center",
-    position: "fixed",
+    position: "center fixed",
     top: "0",
     marginBottom:"20px"
     
