2736d34920ad575cb9c791d60f76d5529ce3c817
pmwritershark <97102702+pmwritershark@users.noreply.github.com>
2023-04-10 14:04:44 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -47,8 +47,9 @@ export function processText(event){
         
     }
   
-    var resultString=JSON.stringify(resultObj,null,2).replace(/{/}/g,'');
+    var resultString=JSON.stringify(resultObj,null,2).replace(/{/g,'');
     resultString=resultString.replace(/"/g,'');
+    resultString=resultString.replace(/}/g,'');
     
     document.getElementById("percentage").value=resultString.replace(/,/g,'\n');
 }
