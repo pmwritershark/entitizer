f691fa5c2dd4eef28a2b5cc59bbd7a79d11b68c2
pmwritershark <97102702+pmwritershark@users.noreply.github.com>
2023-04-10 17:04:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from "./components/Footer";
 import Table from "./components/Table";
-//import Background from './Background';
 
 function App() {
   return (
     <div className="App">
-    //  <Background/>
       <Navbar/>
       <Table/>
       <Footer/>
